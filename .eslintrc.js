391cc348ea21ee58a4aae48ad992d7f84a37c36f
Alexander Maru <i@alexandermaru.com>
2022-04-16 02:06:18 +0000

    configure react base setup

---
+++ .eslintrc.js
@@ -0,0 +1,55 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:import/recommended",
+    "airbnb",
+    "airbnb-typescript",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["react", "@typescript-eslint", "import"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    "import/no-named-as-default": "off",
+    "react/jsx-props-no-spreading": "off",
+    "import/no-unresolved": "error",
+    "import/prefer-default-export": "off",
+    "import/order": [
+      "error",
+      {
+        groups: ["builtin", "external", "internal"],
+        pathGroups: [
+          {
+            pattern: "react",
+            group: "external",
+            position: "before",
+          },
+        ],
+        pathGroupsExcludedImportTypes: ["react"],
+        "newlines-between": "always",
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
+  },
+};
