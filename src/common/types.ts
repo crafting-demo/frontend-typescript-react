79035f4aa473aa862fdbd825a291f9908c4030e0
Alexander Maru <i@alexandermaru.com>
2022-04-24 03:51:16 +0000

    add kafka client

---
+++ src/common/types.ts
@@ -0,0 +1,23 @@
+export enum ActionType {
+  Echo = "Echo",
+  Read = "Read",
+  Write = "Write",
+  Call = "Call",
+}
+
+export enum Topic {
+  React = "queue-react",
+  Golang = "queue-golang",
+  Express = "queue-express",
+  Ruby = "queue-ruby",
+  Kotlin = "queue-kotlin",
+  Python = "queue-python",
+}
+
+export enum Dependency {
+  MySQL = "mysql",
+  Postgres = "postgres",
+  MongoDB = "mongodb",
+  DynamoDB = "dynamodb",
+  Redis = "redis",
+}
